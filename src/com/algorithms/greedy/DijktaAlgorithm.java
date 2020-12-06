394653d57ccd9528eaa6ad9210ef3e8c6c453499
gogagubi <gogagubi@gmail.com>
2020-12-06 21:26:30 +0400

    Greedy -> Dijktra Algorithm

--- src/com/algorithms/greedy/DijktaAlgorithm.java
+++ src/com/algorithms/greedy/DijktaAlgorithm.java
@@ -16,6 +16,24 @@ public class DijktaAlgorithm {
             int n = 6;
             int start = 1;
             int[][] graph = {
+                    {1, 2, 2},
+                    {1, 3, 4},
+                    {2, 3, 1},
+                    {2, 4, 7},
+                    {3, 5, 3},
+                    {4, 6, 1},
+                    {5, 4, 2},
+                    {5, 6, 5}
+            };
+
+            System.out.println("Result " + s.shortestPath(n, start, graph));
+        }
+
+        if (true) {
+            DijktaAlgorithm s = new DijktaAlgorithm();
+            int n = 6;
+            int start = 1;
+            int[][] graph = {
                     {1, 2, 50},
                     {1, 3, 45},
                     {1, 4, 10},
@@ -28,33 +46,48 @@ public class DijktaAlgorithm {
                     {6, 5, 3},
             };
 
-            System.out.println("Prim's Algorithm Says " + s.shortestPath(n, start, graph));
+            System.out.println("Result " + s.shortestPath(n, start, graph));
         }
-
     }
 
     private int shortestPath(int n, int start, int[][] graph) {
-        int ans = 0;
-
         //build map for directed graph
         Map<Integer, List<int[]>> map = buildMap(graph);
 
-        Map<Integer, Integer> paths = new HashMap<>();
+        Map<Integer, Integer> values = new HashMap<>();
         Set<Integer> selected = new HashSet<>();
-
         selected.add(start);
-        //test
 
-        return ans;
+        return dfs(start, 0, map, values, selected);
     }
 
-    private int dfs(){
-        return 0;
+    private int dfs(int key, int val, Map<Integer, List<int[]>> map, Map<Integer, Integer> values, Set<Integer> selected) {
+        int[] minEdge = null;
+        int minCost = Integer.MAX_VALUE;
+        if (!map.containsKey(key)) return val;
+
+        for (int[] i : map.get(key)) {
+            if (!selected.contains(i[1])) {
+                int potentialMin = Math.min(val + i[2], values.getOrDefault(i[1], Integer.MAX_VALUE));
+                if (potentialMin < minCost) {
+                    minEdge = i;
+                    minCost = potentialMin;
+                }
+
+                values.put(i[1], potentialMin);
+            }
+        }
+
+        if (minEdge != null) {
+            selected.add(minEdge[1]);
+            val = dfs(minEdge[1], minCost, map, values, selected);
+        }
+
+        return val;
     }
 
     private Map<Integer, List<int[]>> buildMap(int[][] graph) {
         Map<Integer, List<int[]>> map = new HashMap<>();
-
 
         for (int[] i : graph) {
             List<int[]> l = map.getOrDefault(i[0], new ArrayList<>());
